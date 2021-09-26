546061238ab4c88d691172110b2de2edc126f82c
dmitriysd <80784547+dmitriysd@users.noreply.github.com>
2021-09-26 13:48:15 +0300

    наконец test passed + открывается браузер на весь экран

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -5,8 +5,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class FirstTest {
     @Test
     public void openLKS(){
-        System.setProperty("webdriver.chrome.driver", "C:\\MyProjects\\TestAuto\\chromedriver.exe");
-        WebDriver dr = new ChromeDriver();
-        dr.get("http://spms-ddb-web.spms-develop.apps.ds1-genr01.corp.dev.vtb/");
+        System.setProperty("webdriver.chrome.driver", "C:\\MyProjects\\TestAuto\\chromedriver93.exe");
+                WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://spms-ddb-web.spms-develop.apps.ds1-genr01.corp.dev.vtb/");
     }
 }
