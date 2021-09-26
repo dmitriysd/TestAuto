da723b0ae6f2e1c809dbd86b30a5a569bc009288
dmitriysd <80784547+dmitriysd@users.noreply.github.com>
2021-09-26 13:27:20 +0300

    адрес chromedriver

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,8 +1,12 @@
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTest {
     @Test
     public void openLKS(){
-
+        System.setProperty("webdriver.chrome.driver", "C:\\MyProjects\\TestAuto\\chromedriver.exe");
+        WebDriver dr = new ChromeDriver();
+        dr.get("http://spms-ddb-web.spms-develop.apps.ds1-genr01.corp.dev.vtb/");
     }
 }
