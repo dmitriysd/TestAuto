0759ffcb617638e3a10b3463b2d76136e2e812a4
dmitriysd <80784547+dmitriysd@users.noreply.github.com>
2021-09-27 09:22:46 +0300

    Добавлена авторизация с переходом на главную страницу

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTest {
@@ -9,5 +11,12 @@ public class FirstTest {
                 WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://spms-ddb-web.spms-develop.apps.ds1-genr01.corp.dev.vtb/");
+
+        WebElement inputName = driver.findElement(By.name("username"));
+        inputName.sendKeys("adm");
+        WebElement inputPass = driver.findElement(By.name("password"));
+        inputPass.sendKeys("adm");
+        WebElement inputSubmit = driver.findElement(By.className("jss158"));
+        inputSubmit.submit();
     }
 }
